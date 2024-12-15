2e181b1da7c6babd2e18f47dcd8ba919b724d492
Abdelruhman Askar <askr10.askr@gmail.com>
2024-12-15 16:16:59 +0200

    LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,235 +1,218 @@
 #include <iostream>
 #include <cassert>
 using namespace std;
-class linkedlist
-{
+
+class linkedlist {
 private:
-	struct Node
-	{
-		int item;
-		Node* next;
-	};
-	Node* head;
-	Node* tail;
-	int length;
+    struct Node {
+        int item;
+        Node* next;
+    };
+    Node* head;  // Points to the first node in the list
+    Node* tail;  // Points to the last node in the list
+    int length;  // Tracks the length of the list
+
 public:
-	linkedlist()
-	{
-		tail = head = nullptr;
-		length = 0;
-	}
-	bool isEmpty()
-	{
-		return length == 0;//head == nullptr
-	}
-	void InsertFirst(int element)
-	{
-		Node* newnode = new Node;
-		newnode->item = element;
-		if (isEmpty()) {
-			newnode->next = nullptr;
-			head = tail = newnode;
-		}
-		else
-		{
-			newnode->next = head;
-			head = newnode;
-		}
-		length++;
-	}
-	void InsertLast(int element)
-	{
-		Node* newnode = new Node;
-		newnode->item = element;
-		if (isEmpty()) {
-			newnode->next = nullptr;
-			head = tail = newnode;
-		}
-		else
-		{
-			tail->next = newnode;
-			newnode->next = nullptr;
-			tail = newnode;
-		}
-		length++;
-	}
-	void InsertAtPos(int pos,int element)
-	{
-		if (pos < 0 || pos > length)
-			cout << "Out of Range on InsertAtPos...!\n";
-		else 
-		{
-		Node* newnode = new Node;
-		newnode->item = element;
-		if (pos == 0)
-			InsertFirst(element);
-		else if (pos == length)
-			InsertLast(element);
-		else
-		{
-			Node* cur = head;
-			for (int i = 1; i < pos; i++)
-				cur = cur->next;
-			newnode->next = cur->next;
-			cur->next = newnode;
-			length++;
-		}
-		}
-	}
-	void RemoveFirst()
-	{
-		if (isEmpty())
-			cout << "Can't delete Empty List on RemoveFirst...!\n";
-		else if (length == 1)
-		{
-			delete head;
-			head = tail = nullptr;
-			length--;
-		}
-		else
-		{
-			Node* cur = head;
-			head = head->next;
-			delete cur;
-			length--;
-		}
-	}
-	void RemoveLast()
-	{
-		if (isEmpty())
-			cout << "Can't delete Empty List on RemoveLast...!\n";
-		else if (length == 1)
-		{
-			delete head;
-			head = tail = nullptr;
-			length--;
-		}
-		else
-		{
-			Node* cur = head->next;
-			Node* prv = head;
-			while (cur != tail)
-			{
-				prv = cur;
-				cur = cur->next;
-			}
-			delete cur;
-			prv->next = nullptr;
-			tail = prv;
-			length--;
-		}
-	}
-	void Remove(int element)
-	{
-		if (isEmpty())
-		{
-			cout << "Can't delete Empty List on Remove...!\n";
-			return;
-		}
-		Node* cur, * prv;
-		if (head->item == element)
-		{
-			/*RemoveFirst();*/
-			cur = head;
-			head = head->next;
-			delete cur;
-			length--;
-			if (isEmpty())
-				tail = nullptr;
-		}
-		else
-		{
-			cur = head->next;
-			prv = head;
-			while (cur != nullptr && cur->item != element)
-			{
-				prv = cur;
-				cur = cur->next;
-			}
-			if (cur == nullptr)
-				cout << "The Element Doesn't Exist In The List on Remove...!\n";
-			else
-			{
-				prv->next = cur->next;
-				if (tail == cur)
-				{
-					tail = prv;
-					delete cur;
-					length--;
-				}
-			}
-		}
-	}
-	void reverse()
-	{
-		Node* prv, * cur, * next;
-		prv = nullptr;
-		cur = head;
-		next = cur->next;
-		tail = head;
-		while (next != nullptr)//cur != nullptr
-		{
-			next = cur->next;
-			cur->next = prv;
-			prv = cur;
-			cur = next;
-		}
-		head = prv;
-	}
-	void Search(int element)
-	{
-		Node* cur = head;
-		bool check = 0;
-		int pos = 0;
-		while (cur != nullptr)
-		{
-			if (cur->item == element)
-			{
-				check = 1;
-				break;
-			}
-			cur = cur->next;
-			pos++;
-		}
-		if (check)
-			cout << "The Element Found in Index : " << pos << endl;
-		else
-			cout << "The Element Has not Found on Search...!\n";
-	}
-	void Display()
-	{
-		cout << "Linked List Elements : [ ";
-		Node* cur = head;
-		while (cur != nullptr)
-		{
-			cout << cur->item << " ";
-			cur = cur->next;
-		}
-		cout << "]\n";
-	}
+    linkedlist() {
+        tail = head = nullptr;
+        length = 0;
+    }
+
+    // Checks if the list is empty
+    bool isEmpty() {
+        return length == 0; // head == nullptr
+    }
+
+    // Inserts a new node at the beginning of the list
+    void InsertFirst(int element) {
+        Node* newnode = new Node;
+        newnode->item = element;
+        if (isEmpty()) {
+            newnode->next = nullptr;
+            head = tail = newnode; // List was empty; set head and tail to new node
+        } else {
+            newnode->next = head; // Point new node to current head
+            head = newnode;       // Update head to the new node
+        }
+        length++;
+    }
+
+    // Inserts a new node at the end of the list
+    void InsertLast(int element) {
+        Node* newnode = new Node;
+        newnode->item = element;
+        if (isEmpty()) {
+            newnode->next = nullptr;
+            head = tail = newnode; // List was empty; set head and tail to new node
+        } else {
+            tail->next = newnode;  // Append new node to the tail
+            newnode->next = nullptr;
+            tail = newnode;        // Update tail to the new node
+        }
+        length++;
+    }
+
+    // Inserts a new node at a specific position
+    void InsertAtPos(int pos, int element) {
+        if (pos < 0 || pos > length) {
+            cout << "Out of Range on InsertAtPos...!\n";
+        } else {
+            Node* newnode = new Node;
+            newnode->item = element;
+            if (pos == 0) {
+                InsertFirst(element); // Insert at head
+            } else if (pos == length) {
+                InsertLast(element);  // Insert at tail
+            } else {
+                Node* cur = head;
+                for (int i = 1; i < pos; i++)
+                    cur = cur->next;  // Navigate to the node before the target position
+                newnode->next = cur->next;
+                cur->next = newnode;
+                length++;
+            }
+        }
+    }
+
+    // Removes the first node in the list
+    void RemoveFirst() {
+        if (isEmpty()) {
+            cout << "Can't delete Empty List on RemoveFirst...!\n";
+        } else if (length == 1) { // Special case: single-node list
+            delete head;
+            head = tail = nullptr;
+            length--;
+        } else {
+            Node* cur = head;
+            head = head->next; // Move head to the next node
+            delete cur;        // Delete the old head
+            length--;
+        }
+    }
+
+    // Removes the last node in the list
+    void RemoveLast() {
+        if (isEmpty()) {
+            cout << "Can't delete Empty List on RemoveLast...!\n";
+        } else if (length == 1) { // Special case: single-node list
+            delete head;
+            head = tail = nullptr;
+            length--;
+        } else {
+            Node* cur = head->next;
+            Node* prv = head;
+            while (cur != tail) { // Find the second-to-last node
+                prv = cur;
+                cur = cur->next;
+            }
+            delete cur;          // Delete the tail node
+            prv->next = nullptr; // Update the new tail
+            tail = prv;
+            length--;
+        }
+    }
+
+    // Removes a specific element from the list
+    void Remove(int element) {
+        if (isEmpty()) {
+            cout << "Can't delete Empty List on Remove...!\n";
+            return;
+        }
+        Node* cur, *prv;
+        if (head->item == element) { // Special case: element is at the head
+            cur = head;
+            head = head->next;
+            delete cur;
+            length--;
+            if (isEmpty())
+                tail = nullptr; // Reset tail if list becomes empty
+        } else {
+            cur = head->next;
+            prv = head;
+            while (cur != nullptr && cur->item != element) {
+                prv = cur;
+                cur = cur->next; // Traverse to find the target node
+            }
+            if (cur == nullptr) {
+                cout << "The Element Doesn't Exist In The List on Remove...!\n";
+            } else {
+                prv->next = cur->next; // Unlink the target node
+                if (tail == cur) {    // Update tail if the removed node was the last
+                    tail = prv;
+                }
+                delete cur;
+                length--;
+            }
+        }
+    }
+
+    // Reverses the linked list
+    void reverse() {
+        Node* prv = nullptr;
+        Node* cur = head;
+        Node* next = nullptr;
+        tail = head; // Update tail to the old head
+        while (cur != nullptr) {
+            next = cur->next;  // Save the next node
+            cur->next = prv;   // Reverse the link
+            prv = cur;         // Move previous forward
+            cur = next;        // Move current forward
+        }
+        head = prv; // Update head to the new head
+    }
+
+    // Searches for an element in the list
+    void Search(int element) {
+        Node* cur = head;
+        bool found = false;
+        int pos = 0;
+        while (cur != nullptr) {
+            if (cur->item == element) {
+                found = true;
+                break;
+            }
+            cur = cur->next;
+            pos++;
+        }
+        if (found)
+            cout << "The Element Found in Index : " << pos << endl;
+        else
+            cout << "The Element Has not Found on Search...!\n";
+    }
+
+    // Displays all elements in the list
+    void Display() {
+        cout << "Linked List Elements : [ ";
+        Node* cur = head;
+        while (cur != nullptr) {
+            cout << cur->item << " ";
+            cur = cur->next;
+        }
+        cout << "]\n";
+    }
 };
-int main()
-{
-	linkedlist l;
-	l.InsertFirst(10);
-	l.InsertAtPos(1, 20);
-	l.InsertLast(30);
-	l.InsertLast(40);
-	l.InsertLast(50);
-	l.InsertAtPos(5,60);
-	l.InsertFirst(0);
-	l.Display();
-	l.RemoveFirst();
-	l.RemoveLast();
-	l.Remove(30);
-	l.Display();
-	l.InsertFirst(0);
-	l.InsertAtPos(1, 10);
-	l.Display();
-	l.Remove(10);
-	l.Remove(20);
-	l.Display();
-	l.reverse();
-	l.Display();
-	l.Search(40);
+
+int main() {
+    linkedlist l;
+    l.InsertFirst(10);
+    l.InsertAtPos(1, 20);
+    l.InsertLast(30);
+    l.InsertLast(40);
+    l.InsertLast(50);
+    l.InsertAtPos(5, 60);
+    l.InsertFirst(0);
+    l.Display();
+    l.RemoveFirst();
+    l.RemoveLast();
+    l.Remove(30);
+    l.Display();
+    l.InsertFirst(0);
+    l.InsertAtPos(1, 10);
+    l.Display();
+    l.Remove(10);
+    l.Remove(20);
+    l.Display();
+    l.reverse();
+    l.Display();
+    l.Search(40);
 }
